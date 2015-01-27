6ad8a591f62afdead4778b88b3353eb3c78d2d11
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2015-01-27 20:06:02 +0100

    add boilerplate google test

---
+++ e2e-tests/storys/googleSpec.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('mit protractor bei google suchen', function() {
+
+    browser.get('http://www.google.com');
+
+
+    it('should show a search field', function () {
+        //expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    });
+
+    it('should show search results on click', function() {
+
+    })
+
+
+
+
+
+});
